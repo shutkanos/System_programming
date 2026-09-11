c431bb501178fe3aa062673f582c3989af9c2c8d
shutkanos <k4r8mgy1kgkf@mail.ru>
2026-09-11 09:40:12 +0300

    Update

--- Lab_1/README.md
+++ Lab_1/README.md
@@ -24,7 +24,7 @@ sudo apt install -y gcc gdb fasm mc
 ```bash
 cd sandbox
 fasm Дегтярев.asm
-chmod +x Дегтярев
+ld -m elf_i386 Дегтярев.o -o Дегтярев
 ./Дегтярев
 ```
 
--- Lab_1/sandbox/Дегтярев.asm
+++ Lab_1/sandbox/Дегтярев.asm
@@ -1,34 +1,15 @@
-format ELF executable 3
-entry start
+format ELF
+public _start
+msg db "Дегтярев", 0xA, "Илья", 0xA, "Сергеевич", 0xA, 0
 
-segment readable executable
-start:
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
-    mov ecx, surname
-    mov edx, surname_len
-    int 0x80
-
+_start:
+    ;инициализация регистров для вывода информации на экран
     mov eax, 4
     mov ebx, 1
-    mov ecx, name
-    mov edx, name_len
+    mov ecx, msg
+    mov edx, 46
     int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, patronymic
-    mov edx, patronymic_len
+    ;инициализация регистров для успешного завершения работы программы
+    mov eax, 1
+    mov ebx, 0
     int 0x80
-
-    mov eax, 1          ; sys_exit
-    xor ebx, ebx
-    int 0x80
-
-segment readable writeable
-surname db 'Дегтярев', 10
-surname_len = $ - surname
-name db 'Илья', 10
-name_len = $ - name
-patronymic db 'Сергеевич', 10
-patronymic_len = $ - patronymic
